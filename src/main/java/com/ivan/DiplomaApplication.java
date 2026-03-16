0358792e8fd90593553347000e5a6496af09105b
Warded120 <greenvania11@gmail.com>
2026-03-16 18:25:43 +0200

    add TODO

--- src/main/java/com/ivan/DiplomaApplication.java
+++ src/main/java/com/ivan/DiplomaApplication.java
@@ -1,12 +1,12 @@
 package com.ivan;
 
+import lombok.NoArgsConstructor;
 import org.apache.camel.main.Main;
 
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public final class DiplomaApplication {
 
-    private DiplomaApplication() {
-    }
-
+    //TODO: add tests
     public static void main(String[] args) throws Exception {
         Main main = new Main(DiplomaApplication.class);
         main.run(args);
