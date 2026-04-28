52ac15baaaac135589c23c0fe4fc0832c238fb45
Warded120 <greenvania11@gmail.com>
2026-04-28 13:06:53 +0300

    fix upgrade.ps1

--- helm/upgrade.ps1
+++ helm/upgrade.ps1
@@ -1,5 +1,5 @@
-helm upgrade diploma-inbound ./inbound-processor --namespace diploma-app --set replicaCount=4
+helm upgrade diploma-outbound ./outbound-processor --namespace diploma-app --set replicaCount=2
 
-helm history diploma-inbound -n diploma-app
+helm history diploma-outbound -n diploma-app
 
-helm rollback diploma-inbound <version> -n diploma-app
+helm rollback diploma-outbound N -n diploma-app
