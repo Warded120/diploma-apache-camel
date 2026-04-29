1294eeb1af2b907d39c5674f5b7f1259c5a23ba7
Warded120 <greenvania11@gmail.com>
2026-04-29 09:05:28 +0300

    add headers to create entities

--- inbound-processor/src/main/java/com/ivan/inbound/constants/ExchangeConstants.java
+++ inbound-processor/src/main/java/com/ivan/inbound/constants/ExchangeConstants.java
@@ -6,6 +6,8 @@ import lombok.experimental.UtilityClass;
 public class ExchangeConstants {
     public static final String HEADER_ID = "id";
     public static final String HEADER_ACTION = "action";
+    public static final String HEADER_CREATE_CUSTOMER = "createCustomer";
+    public static final String HEADER_CREATE_PRODUCT = "createProduct";
     public static final String HEADER_VALIDATION_ERROR = "ValidationError";
 }
 
--- inbound-processor/src/main/java/com/ivan/inbound/route/CreateOrderRoute.java
+++ inbound-processor/src/main/java/com/ivan/inbound/route/CreateOrderRoute.java
@@ -8,6 +8,8 @@ import org.apache.camel.component.bean.validator.BeanValidationException;
 import org.apache.camel.model.dataformat.JsonLibrary;
 
 import static com.ivan.inbound.constants.ExchangeConstants.HEADER_ACTION;
+import static com.ivan.inbound.constants.ExchangeConstants.HEADER_CREATE_CUSTOMER;
+import static com.ivan.inbound.constants.ExchangeConstants.HEADER_CREATE_PRODUCT;
 import static com.ivan.inbound.constants.RouteConstants.CREATE_ORDER_ROUTE;
 import static com.ivan.inbound.constants.RouteConstants.CREATE_ORDER_ROUTE_ID;
 import static com.ivan.inbound.constants.RouteConstants.ORDER_DTO_VALIDATOR;
@@ -15,7 +17,6 @@ import static com.ivan.inbound.enumeration.OrderAction.CREATE;
 import static com.ivan.inbound.util.ClassUtil.target;
 
 public class CreateOrderRoute extends EndpointRouteBuilder {
-
 
     @Override
     public void configure() {
@@ -34,7 +35,7 @@ public class CreateOrderRoute extends EndpointRouteBuilder {
             .unmarshal().json(JsonLibrary.Jackson, OrderDto.class)
             .to(beanValidator(ORDER_DTO_VALIDATOR))
             .to(mapstruct(target(OrderMessage.class)))
-            .removeHeaders("*")
+            .removeHeaders("*", HEADER_CREATE_CUSTOMER, HEADER_CREATE_PRODUCT)
             .setHeader(HEADER_ACTION, constant(CREATE.getAction()))
             .to(kafka("{{kafka.topic}}"))
             .setBody(simple("Order request is created"))
