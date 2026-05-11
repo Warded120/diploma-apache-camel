12f3740e7a9368e331fe54280ad218e621cc09af
Warded120 <greenvania11@gmail.com>
2026-05-11 16:56:40 +0300

    remove k8s and helm folders

--- helm/upgrade.ps1
+++
@@ -1,5 +0,0 @@
-helm upgrade diploma-outbound ./outbound-processor --namespace diploma-app --set replicaCount=2
-
-helm history diploma-outbound -n diploma-app
-
-helm rollback diploma-outbound N -n diploma-app
